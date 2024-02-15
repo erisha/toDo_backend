ad007791bfa79873ff9ec66056ea6923b6084612
Erisha Roach <forever.erisha@gmail.com>
2024-02-15 13:38:25 -0500

    having an error connecting routes

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
- const cors = require('cors')
+import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// import passport from 'passport';
+import session from 'express-session';
+
+//import routes
+import usersRouter from '../toDo_backend/routes/userRoutes'
+import todoRouter from '../toDo_backend/routes/todoRoutes'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middleware
-app.use(morgan('dev'));//logger
+app.use(morgan('dev'));//logger shows request being called
 app.use(cors());// connect to frontend
-app.use(express.json())
+app.use(express.json())// date in req.body
+app.use(express.urlencoded({ extended: true })); // allow data in url
+app.use(bodyParser.json());
 
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+app.use('api/users', usersRouter)
+app.use('api/todo', todoRouter)
+mongoose.connect('mongodb://127.0.0.1:27017/Todo');
 
 //routes
 
-
-
-//user
 //randomActivity
 
 
